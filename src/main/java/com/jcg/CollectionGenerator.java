d22676b514f049716d66ddf983732f5bcdacac59
Tyler Yates <tyler.yates@utexas.edu>
2015-01-15 01:45:48 -0600

    Add list and set generation methods that take in a size parameter.

--- src/main/java/com/jcg/CollectionGenerator.java
+++ src/main/java/com/jcg/CollectionGenerator.java
@@ -1,9 +1,6 @@
 package com.jcg;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Used to generate various collection instances.
@@ -18,11 +15,20 @@ public class CollectionGenerator {
      * @return the created non-empty list
      */
     public static <T> List<T> generateList(ElementEmitter<T> elementEmitter) {
-        final List<T> list = new ArrayList<T>();
-        for (int i = 0; i < 10; i++) {
-            list.add(elementEmitter.emitElement());
-        }
+        return generateList(elementEmitter, 10);
+    }
 
+    /**
+     * Generates a list using the given {@link com.jcg.ElementEmitter} to generate the given number of elements in the list.
+     *
+     * @param elementEmitter the element emitter
+     * @param numElements    the number of elements the list will contain
+     * @param <T>            the type of elements that the list will contain
+     * @return the created list
+     */
+    public static <T> List<T> generateList(ElementEmitter<T> elementEmitter, int numElements) {
+        final List<T> list = new ArrayList<T>();
+        generateCollection(list, elementEmitter, numElements);
         return list;
     }
 
@@ -30,15 +36,30 @@ public class CollectionGenerator {
      * Generates a non-empty set using the given {@link com.jcg.ElementEmitter} to generate the elements in the set.
      *
      * @param elementEmitter the element emitter
-     * @param <T>            the type of elements that the list will contain
+     * @param <T>            the type of elements that the set will contain
      * @return the created non-empty set
      */
     public static <T> Set<T> generateSet(ElementEmitter<T> elementEmitter) {
-        final Set<T> set = new HashSet<T>();
-        while (set.size() < 10) {
-            set.add(elementEmitter.emitElement());
-        }
+        return generateSet(elementEmitter, 10);
+    }
 
+    /**
+     * Generates a set using the given {@link com.jcg.ElementEmitter} to generate the elements in the set.
+     *
+     * @param elementEmitter the element emitter
+     * @param numElements    the number of elements the set will contain
+     * @param <T>            the type of elements that the set will contain
+     * @return the created set
+     */
+    public static <T> Set<T> generateSet(ElementEmitter<T> elementEmitter, int numElements) {
+        final Set<T> set = new HashSet<T>();
+        generateCollection(set, elementEmitter, numElements);
         return set;
+    }
+
+    private static <T> void generateCollection(Collection<T> collection, ElementEmitter<T> elementEmitter, int numElements) {
+        while (collection.size() < numElements) {
+            collection.add(elementEmitter.emitElement());
+        }
     }
 }
--- src/test/java/com/jcg/CollectionGeneratorTest.java
+++ src/test/java/com/jcg/CollectionGeneratorTest.java
@@ -5,19 +5,33 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class CollectionGeneratorTest {
+    private final int SIZE = 100;
 
     @Test
-    public void testListGeneration() {
+    public void testGenerateList() throws Exception {
         final List<Integer> list = CollectionGenerator.generateList(ElementEmitters.POSITIVE_INTEGERS);
         assertFalse(list.isEmpty());
     }
 
     @Test
-    public void testSetGeneration() {
+    public void testGenerateListWithSize() throws Exception {
+        final List<Integer> list = CollectionGenerator.generateList(ElementEmitters.POSITIVE_INTEGERS, SIZE);
+        assertEquals(SIZE, list.size());
+    }
+
+    @Test
+    public void testGenerateSet() throws Exception {
         final Set<Integer> set = CollectionGenerator.generateSet(ElementEmitters.POSITIVE_INTEGERS);
         assertFalse(set.isEmpty());
+    }
+
+    @Test
+    public void testGenerateSetWithSize() throws Exception {
+        final Set<Integer> set = CollectionGenerator.generateSet(ElementEmitters.POSITIVE_INTEGERS, SIZE);
+        assertEquals(SIZE, set.size());
     }
 }
